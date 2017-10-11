7045b71f4d9e34d2d09322ed845da4ac74a65139
sanketswagh <sanket.wagh@quantiphi.com>
2017-10-11 15:43:01 +0530

    All programs completed 11-10-2017 15:42

--- Assignment_1(Python_Assignment)/question10.py
+++ Assignment_1(Python_Assignment)/question10.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Oct 09 16:27:51 2017
+Created on Wed Oct 11 15:24:32 2017
 
 @author: Sanket Wagh
 """
-#import pytz
+import pytz
 import datetime
-#gmt = pytz.timezone('GMT')
 
-currentdatetime = datetime.datetime.now()
-#time = currentdatetime
-#date = datetime.datetime.strptime(time, '%a, %d %b %Y %H:%M:%S GMT')
-print("Current date & time(IST): ",currentdatetime)
+gmt = pytz.timezone('GMT')
+eastern = pytz.timezone('US/Eastern')
 
+time = "Tue, 11 Oct 2017 15:38:10 GMT"
 
-#eastern = pytz.timezone('US/Eastern')
-#time = currentdatetime
-#date = datetime.datetime.strptime(time, '%a, %d %b %Y %H:%M:%S GMT')
-#dategmt = gmt.localize(date)
-#dateeastern = dategmt.astimezone(eastern)
+date = datetime.datetime.strptime(time, '%a, %d %b %Y %H:%M:%S GMT')
+
+dategmt = gmt.localize(date)
+
+dateeastern = dategmt.astimezone(eastern)
+dateindian = dategmt.astimezone(gmt)
+
+print "IST format (GMT): ",dateindian
+print "US format(EST): ",dateeastern
