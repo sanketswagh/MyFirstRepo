03394e32f8d27212947cde96e5e1d6aeb77ac1b4
sanketswagh <sanket.wagh@quantiphi.com>
2017-10-11 11:46:43 +0530

    latest changes 11-10-2017 11:46

--- Assignment_1(Python_Assignment)/question12.py
+++ Assignment_1(Python_Assignment)/question12.py
@@ -7,7 +7,11 @@ Created on Tue Oct 10 16:35:30 2017
 import datetime
 import logging
 class AdditiveSequenceCheck:
-    try:    
+    try:
+        currentdatetime = datetime.datetime.now()
+        LOG_FORMAT = "%(levelname)s %(asctime)s %(message)s"
+        logging.basicConfig(filename="question12.log",filemode='w',level=logging.DEBUG,format=LOG_FORMAT)
+        logger = logging.getLogger()
         def additiveSequence(seq,nTerm):
                 
               list1 = [int(i) for i in str(seq)]
@@ -29,13 +33,10 @@ class AdditiveSequenceCheck:
             main()
         
         seq = raw_input("Enter the sequence: ")
+        logging.info("Sequence entered")
         nTerm = input("Enter the Nth term: ")
+        logging.info("Entered the Nth term")
+        logging.debug("#Called the additiveSequence method")
         additiveSequence(seq,nTerm)
-        
-        currentdatetime = datetime.datetime.now()
-        logging.basicConfig(filename='C:\Program Files\Git\MyFirstRepo\Assignment1\question12.log',filemode='w',level=logging.DEBUG)
-        logging.debug('This message should go to the log file',currentdatetime)
-        logging.warning('Entry made into log file....!!!')
-            
     except Exception, e:
         print "Exception Handled Successfully : ", e
