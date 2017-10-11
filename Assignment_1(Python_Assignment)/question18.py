628502ad5ef9eb77ef8efe50f3935d89d57eadcc
sanketswagh <sanket.wagh@quantiphi.com>
2017-10-11 10:01:39 +0530

    latest changes 11-10-2017 10:01

---
+++ Assignment_1(Python_Assignment)/question18.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 11 09:47:04 2017
+
+@author: Sanket Wagh
+"""
+
+class ListDemo:
+    list1 = []
+    try:    
+        def getList(self):
+            print "List is: ",list1
+                        
+        def setList(self,list1):
+            ListDemo.list1.append(list1)
+        
+#        def removeList(self,element):
+#            del list1[element]
+                        
+    except Exception, e:
+        print "Exception Handled Successfully : ", e
+l = ListDemo()
+list1 = raw_input("Enter the elements: ")
+l.setList(list1)
+l.getList()
+#element = int(raw_input("Enter the element to remove: "))
+#l.removeList(element)
